e18ae274bac6d6f8635f705fc6bc75e43ee286fd
azerpas <manikhouth.anthony@gmail.com>
2023-12-29 17:20:23 +0100

    fix: claim token program field (rm TODO)

--- programs/cardinal-token-manager/src/instructions/claim.rs
+++ programs/cardinal-token-manager/src/instructions/claim.rs
@@ -173,7 +173,7 @@ pub fn handler<'key, 'accounts, 'remaining, 'info>(ctx: Context<'key, 'accounts,
                     token_account: ctx.accounts.recipient_token_account.key(),
                     edition: *edition_info.key,
                     mint: ctx.accounts.mint.key(),
-                    token_program: spl_token::ID, // TODO: unsure
+                    token_program: spl_token::ID,
                 }.instruction(),
                 &[
                     token_manager.to_account_info(),
